45e55c3d32b625cb96bccacfdb477e530e06ef07
himanshu321jsr <2022ugee040@nitjsr.ac.in>
2025-07-11 00:50:25 +0530

    Update messageSlice.js

--- frontend/src/Redux/messageSlice.js
+++ frontend/src/Redux/messageSlice.js
@@ -1,23 +1,4 @@
-// import { createSlice } from '@reduxjs/toolkit';
 
-// const messageSlice = createSlice({
-//     name: 'message',
-//     initialState: {
-//         messages: [],
-//     },
-//     reducers: {
-//         setMessages: (state, action) => {
-//             state.messages = action.payload;
-//         },
-//         addMessage: (state, action) => {
-//             state.messages.push(action.payload);
-//         },
-        
-
-//     }
-// });
-// export const { setMessages, addMessage } = messageSlice.actions;
-// export default messageSlice.reducer;
 
 import { createSlice } from '@reduxjs/toolkit';
 
