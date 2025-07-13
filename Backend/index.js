4a6a65835d92616f04953508abd4fd21ab1964e7
zafarEqbal <zafareqbalahmad08029@gmail.com>
2025-07-14 03:42:04 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 const corsoption = {
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-application-frontend-ii88.onrender.com',
     credentials: true,
 };
 app.use(cors(corsoption));
